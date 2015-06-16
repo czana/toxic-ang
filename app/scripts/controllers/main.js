a0597a816d7125c098e059df38b64de50b466f2e
tc <tomasz.czana@gmail.com>
2015-06-16 02:00:45 +0200

    Add counter

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,8 +11,17 @@ angular.module('toxicApp')
   .controller('MainCtrl', function ($scope, $timeout) {
   	var rootRef = new Firebase('https://brilliant-fire-9601.firebaseio.com/');
   	var roomsRef = rootRef.child('rooms');
+    var counter = null;
 
   	$scope.rooms = [];
+
+    $scope.onCount = function() {
+      for(var i = 0; i < $scope.rooms.length; i++) {
+        $scope.rooms[i].time += 1000;
+      }
+
+      counter = $timeout($scope.onCount, 1000);
+    }
 
   	roomsRef.on('child_added', function(snapshot) {
   		$timeout(function() {
@@ -20,8 +29,12 @@ angular.module('toxicApp')
 
   			$scope.rooms.push({
   				empty: snapshotVal.empty,
+          time: 0,
   				key: snapshot.key()
   			});
+
+        if(!counter)
+          counter = $timeout($scope.onCount, 1000);
   		});
   	});
 
@@ -30,6 +43,7 @@ angular.module('toxicApp')
   			var snapshotVal = snapshot.val();
   			var room = $.grep($scope.rooms, function(room) { return room.key == snapshot.key(); })[0];
   			room.empty = snapshotVal.empty;
+        room.time = 0;
   		});
   	});
 
@@ -39,7 +53,7 @@ angular.module('toxicApp')
   		});
   	};
 
-    $scope.setClass = function(room) {
+    $scope.getClass = function(room) {
       return (room.empty == 1) ? 'empty' : 'busy';
     };
 
